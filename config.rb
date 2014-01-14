eeed3153818edad6ecc3d081b34ae330e7c7c16c
Timur Vafin <timur.vafin@flatstack.com>
2014-01-14 13:51:15 +0400

    Mimic Rails assets folder structure

--- config.rb
+++ config.rb
@@ -1,3 +1,9 @@
+set :js_dir, 'assets/javascripts'
+set :css_dir, 'assets/stylesheets'
+set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
+set :partials_dir, 'partials'
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
