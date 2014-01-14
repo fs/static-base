d43e12a2d0d9ef2630ce6a95e55d77b697f30a90
Timur Vafin <timur.vafin@flatstack.com>
2014-01-14 11:30:13 +0300

    Cleanup i18n silence
    - Remove `::`
    - Mention link to discussion

--- config.rb
+++ config.rb
@@ -9,5 +9,5 @@ activate :deploy do |deploy|
   deploy.method = :git
 end
 
-# silence i18n warning
-::I18n.config.enforce_available_locales = false
+# Silence I18n warning, http://stackoverflow.com/a/20381730
+I18n.config.enforce_available_locales = false
