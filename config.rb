bc21de0d56d4be12b1acd5fbdfba44bf0ed95cbe
Timur Vafin <timur.vafin@flatstack.com>
2014-01-15 19:40:59 +0400

    Activate relative assets
    We need to have assets linked relatively to be able to host generated
    content as http://fs.github.io/static-base

--- config.rb
+++ config.rb
@@ -8,6 +8,7 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
+  activate :relative_assets
 
   # Only Including Tracking Code in Builds
   activate :google_analytics do |ga|
